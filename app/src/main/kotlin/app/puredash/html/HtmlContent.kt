289276d796673a28ac52354718021593c1a0066c
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2025-11-11 19:55:44 +0100

    make page auto update every 5 minutes

--- app/src/main/kotlin/app/puredash/html/HtmlContent.kt
+++ app/src/main/kotlin/app/puredash/html/HtmlContent.kt
@@ -11,6 +11,7 @@ fun getHtmlContent(
     head {
         meta(charset = "UTF-8")
         meta(name = "viewport", content = "width=device-width, initial-scale=1.0")
+        meta(name = "refresh", content = "300")
         title("PureDash")
         style {
             unsafe {
